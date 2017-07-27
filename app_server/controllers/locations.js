c42c1cb0fcc93b71ef8f20c2f0ced16740dab50e
h3llofr1end <yuriy.dotsya@gmail.com>
2017-07-27 11:27:45 -0700

    lesson 4 + homework

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -33,9 +33,51 @@ module.exports.homelist = function(req, res) {
 };
 
 module.exports.locationInfo = function(req, res) {
-	res.render('location-info', {title: 'Location info'});
+	res.render('location-info', {
+		title: 'Сушия',
+		pageHeader: {title: 'Сушия'},
+		sidebar: {
+			context: 'Сушия на Loc8r потому что это прекрасное время где можно провести время и заняться продуктивной работой.',
+			callToAction: 'Если вы здесь были и вам понравилось, ну или нет - пожалуйста оставьте отзыв, чтобы помочь другим пользователям.'
+		},
+		location: {
+			title: 'Сушия',
+			address: 'Невский проспект, 108',
+			rating: 3,
+			facilities: ['Горячие напитки', 'Вкусные роллы', 'Хороший вай-фай'],
+			coords: {lat: 59.9317527, lng: 30.355245},
+			openingTimes: [{
+				days: 'понедельник - пятница',
+				opening: '07:00',
+				closing: '19:00',
+				closed: false
+			},{
+				days: 'суббота',
+				opening: '08:00',
+				closing: '17:00',
+				closed: false
+			}, {
+				days: 'воскресенье',
+				closed: true
+			}],
+			reviews: [{
+				author: 'Иван Иванов',
+				rating: 5,
+				timestamp : '22.07.2017',
+				reviewText: 'Какое чудное место. Я обязательно приду сюда еще раз!',
+			}, {
+				author: 'Петр Петров',
+				rating: 3,
+				timestamp : '22.06.2017',
+				reviewText: 'Всё было хорошо. Кофе не был хорошим, зато вай-фай шикарен.',
+			}]
+		}
+	});
 };
 
 module.exports.addReview = function(req, res) {
-	res.render('location-review-form', {title: 'Add review'});
+	res.render('location-review-form', {
+		title: 'Добавить обзор на "Сушия" на Loc8r',
+		pageHeader: {title: 'Добавить обзор на "Сушия"'}
+	});
 };
--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -1,3 +1,6 @@
 module.exports.about = function(req, res) {
-	res.render('generic-text', {title: 'About'});
+	res.render('generic-text', {
+		title: 'О проекте Loc8r',
+		content: 'Loc8r был создан чтобы помочь людям найти место где можно приятно провести время\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit Nunc sed lorem ac nisi dignissim accumsan.'
+	});
 };
